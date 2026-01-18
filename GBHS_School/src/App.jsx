89742b93f20ed2372653a39155e8384be5f8b0e7
M Awais <khanmowais5@gmail.com>
2026-01-18 20:43:23 +0500

    import the SubjectFaculty Page

--- GBHS_School/src/App.jsx
+++ GBHS_School/src/App.jsx
@@ -3,10 +3,10 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Gallery from './pages/Gallery'
-// import Registration from './pages/Registration'
+import Registration from './pages/Registration'
 import Contact from './pages/Contact'
 import FacultyList from './pages/FacultyList'
-// import SubjectFaculty from './pages/SubjectFaculty'
+import SubjectFaculty from './pages/SubjectFaculty'
 
 function App() {
   return (
@@ -14,12 +14,12 @@ function App() {
       {/* <Route path="/" element={<Layout />}> */}
         <Route index element={<Home />} />
          <Route path="about" element={<About />} />
-        {/* <Route path="gallery" element={<Gallery />} /> */}
-        {/* <Route path="registration" element={<Registration />} /> */}
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="registration" element={<Registration />} />
         <Route path="contact" element={<Contact />} />
         <Route path="faculty" element={<FacultyList />} />
-        {/* <Route path="faculty/:subject" element={<SubjectFaculty />} /> */}
-      {/* </Route>  */}
+        <Route path="faculty/:subject" element={<SubjectFaculty />} />
+     
     </Routes>
   )
 }
